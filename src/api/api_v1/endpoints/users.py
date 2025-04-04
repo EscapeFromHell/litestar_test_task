b6b22670293ba3a69a2718c31a6d75849054bb3f
EscapeFromHell <kirill199752@yandex.ru>
2025-04-04 15:21:11 +0400

    fix endpoints

--- src/api/api_v1/endpoints/users.py
+++ src/api/api_v1/endpoints/users.py
@@ -7,27 +7,27 @@ from src.core.service import UsersService
 from src.core.uow import UnitOfWork
 
 
-@post("/", status_code=201, dependencies={"uow": Provide(UnitOfWork)})
+@post("/", status_code=201, dependencies={"uow": Provide(UnitOfWork, sync_to_thread=False)})
 async def create_user(data: UserCreate, uow: UnitOfWork = Dependency()) -> User:
     return await UsersService.create_user(user=data, uow=uow)
 
 
-@get("/", status_code=200, dependencies={"uow": Provide(UnitOfWork)})
-async def get_users(uow: UnitOfWork) -> list[User]:
+@get("/", status_code=200, dependencies={"uow": Provide(UnitOfWork, sync_to_thread=False)})
+async def get_users(uow: UnitOfWork = Dependency()) -> list[User]:
     return await UsersService.get_users(uow=uow)
 
 
-@get("/{user_id:int}", status_code=200, dependencies={"uow": Provide(UnitOfWork)})
+@get("/{user_id:int}", status_code=200, dependencies={"uow": Provide(UnitOfWork, sync_to_thread=False)})
 async def get_user(user_id: int, uow: UnitOfWork = Dependency()) -> User:
     return await UsersService.get_user(user_id=user_id, uow=uow)
 
 
-@put("/{user_id:int}", status_code=200, dependencies={"uow": Provide(UnitOfWork)})
+@put("/{user_id:int}", status_code=200, dependencies={"uow": Provide(UnitOfWork, sync_to_thread=False)})
 async def update_user(user_id: int, data: UserUpdate, uow: UnitOfWork = Dependency()) -> User:
     return await UsersService.update_user(user_id=user_id, user=data, uow=uow)
 
 
-@delete("/{user_id:int}", status_code=200, dependencies={"uow": Provide(UnitOfWork)})
+@delete("/{user_id:int}", status_code=200, dependencies={"uow": Provide(UnitOfWork, sync_to_thread=False)})
 async def delete_user(user_id: int, uow: UnitOfWork = Dependency()) -> dict:
     return await UsersService.delete_user(user_id=user_id, uow=uow)
 
